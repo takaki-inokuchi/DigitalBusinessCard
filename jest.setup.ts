086e8336d91fa713548f787d857e783bdbc691e3
井ノ口 孝輝 <inokutitakaki@yahoo.co.jp>
2025-09-27 19:44:21 +0900

    test

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,5 +1,11 @@
 import "@testing-library/jest-dom";
 import { TextEncoder, TextDecoder } from "util";
 
-(global as any).TextEncoder = TextEncoder;
-(global as any).TextDecoder = TextDecoder;
+// 型キャストして global に代入
+if (typeof global.TextEncoder === "undefined") {
+  (global as any).TextEncoder = TextEncoder;
+}
+
+if (typeof global.TextDecoder === "undefined") {
+  (global as any).TextDecoder = TextDecoder;
+}
