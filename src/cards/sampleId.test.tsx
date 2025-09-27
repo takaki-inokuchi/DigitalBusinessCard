d53ed2e861cb4405fe9fe5effaffbc6a483d7866
井ノ口 孝輝 <inokutitakaki@yahoo.co.jp>
2025-09-27 21:43:59 +0900

    test

--- src/cards/sampleId.test.tsx
+++ src/cards/sampleId.test.tsx
@@ -2,17 +2,17 @@ import { render, screen, fireEvent } from "@testing-library/react";
 import { MemoryRouter, Route, Routes } from "react-router-dom";
 import { SampleId } from "./sample-id";
 
-const mockUser = {
-  user_id: "test123",
-  name: "井ノ口孝輝",
-  description: "自己紹介テキスト",
-  github_id: "https://github.com/test",
-  qiita_id: "https://qiita.com/test",
-  x_id: "https://x.com/test",
-  created_at: "2025-01-01",
-};
 //データベールへのアクセスをモック化
 jest.mock("../../supabaseClient", () => ({
+  mockUser: {
+    user_id: "test123",
+    name: "井ノ口孝輝",
+    description: "自己紹介テキスト",
+    github_id: "https://github.com/test",
+    qiita_id: "https://qiita.com/test",
+    x_id: "https://x.com/test",
+    created_at: "2025-01-01",
+  },
   supabase: {
     from: jest.fn().mockReturnThis(),
     select: jest.fn().mockReturnThis(),
