04a688316fd06e6dfa20dd84676495c1e636d63e
井ノ口 孝輝 <inokutitakaki@yahoo.co.jp>
2025-09-27 21:58:34 +0900

    test

---
+++ src/tests/mockUser.ts
@@ -0,0 +1,9 @@
+export const mockUser = {
+  user_id: "test123",
+  name: "井ノ口孝輝",
+  description: "自己紹介テキスト",
+  github_id: "https://github.com/test",
+  qiita_id: "https://qiita.com/test",
+  x_id: "https://x.com/test",
+  created_at: "2025-01-01",
+};
