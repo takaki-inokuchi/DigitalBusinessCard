56b78b0e5f815f022a0ede885d3c38eeef7a9c26
井ノ口 孝輝 <inokutitakaki@yahoo.co.jp>
2025-09-28 17:05:52 +0900

    kousinn

---
+++ batch/index.ts
@@ -0,0 +1,27 @@
+import { supabase } from "../supabaseClient.ts";
+
+async function deleteYesterdayData() {
+  const yesterday = new Date();
+  yesterday.setDate(yesterday.getDate() - 1);
+  const yyyymmdd = yesterday.toISOString().split("T")[0];
+
+  // users 削除
+  const { error: userErr } = await supabase
+    .from("users")
+    .delete()
+    .gte("created_at", `${yyyymmdd}T00:00:00Z`)
+    .lt("created_at", `${yyyymmdd}T23:59:59Z`);
+  if (userErr) console.error("users削除エラー:", userErr);
+
+  // user_skill 削除 途中でskillsをusersにまとめたため、使用せず
+  // const { error: skillErr } = await supabase
+  //   .from("user_skill")
+  //   .delete()
+  //   .gte("created_at", `${yyyymmdd}T00:00:00Z`)
+  //   .lt("created_at", `${yyyymmdd}T23:59:59Z`);
+  // if (skillErr) console.error("user_skill削除エラー:", skillErr);
+
+  console.log("前日データ削除完了:", yyyymmdd);
+}
+
+deleteYesterdayData();
--- supabaseClient.ts
+++ supabaseClient.ts
@@ -1,9 +1,20 @@
 import { createClient } from "@supabase/supabase-js";
+import dotenv from "dotenv";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-console.log("SUPABASE_URL:", import.meta.env.VITE_SUPABASE_URL);
-console.log("SUPABASE_KEY:", import.meta.env.VITE_SUPABASE_ANON_KEY);
+dotenv.config();
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+// const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+// const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+// console.log("SUPABASE_URL:", import.meta.env.VITE_SUPABASE_URL);
+// console.log("SUPABASE_KEY:", import.meta.env.VITE_SUPABASE_ANON_KEY);
+
+// export const supabase = createClient(supabaseUrl, supabaseKey);
+
+const supabaseUrl =
+  process.env.SUPABASE_URL || import.meta.env?.VITE_SUPABASE_URL;
+const supabaseKey =
+  process.env.SUPABASE_SERVICE_ROLE_KEY || import.meta.env?.VITE_SUPABASE_ANON_KEY;
+
+export const supabase = createClient(supabaseUrl!, supabaseKey!);
