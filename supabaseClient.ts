b59eca838b652af3d72a235aad0731570cdfccc3
井ノ口 孝輝 <inokutitakaki@yahoo.co.jp>
2025-09-28 17:17:25 +0900

    6時更新

--- supabaseClient.ts
+++ supabaseClient.ts
@@ -1,16 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 import dotenv from "dotenv";
 
-
-dotenv.config();
-
-// const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-// const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
-
-// console.log("SUPABASE_URL:", import.meta.env.VITE_SUPABASE_URL);
-// console.log("SUPABASE_KEY:", import.meta.env.VITE_SUPABASE_ANON_KEY);
-
-// export const supabase = createClient(supabaseUrl, supabaseKey);
+if (typeof window === "undefined") {
+  dotenv.config();
+}
 
 const supabaseUrl =
   process.env.SUPABASE_URL || import.meta.env?.VITE_SUPABASE_URL;
